c345c03d9ffb808634163f8b94d43c8643333ae0
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-02-23 11:30:08 +0000

    added lesson 10 end code

--- index.js
+++ index.js
@@ -15,6 +15,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', require('./routes/api'));
 
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requests');
--- routes/api.js
+++ routes/api.js
@@ -3,24 +3,24 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(req, res){
+router.get('/ninjas', function(req, res, next){
     res.send({type: 'GET'});
 });
 
 // add a new ninja to the db
-router.post('/ninjas', function(req, res){
+router.post('/ninjas', function(req, res, next){
     Ninja.create(req.body).then(function(ninja){
         res.send(ninja);
-    });
+    }).catch(next);
 });
 
 // update a ninja in the db
-router.put('/ninjas/:id', function(req, res){
+router.put('/ninjas/:id', function(req, res, next){
     res.send({type: 'PUT'});
 });
 
 // delete a ninja from the db
-router.delete('/ninjas/:id', function(req, res){
+router.delete('/ninjas/:id', function(req, res, next){
     res.send({type: 'DELETE'});
 });
 
