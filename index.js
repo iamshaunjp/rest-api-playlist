910b5dd446a1312ed369dd386231fdfbc2852249
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-02-23 10:05:48 +0000

    added lesson 5 end code

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express');
 // set up express app
 const app = express();
 
+app.get('/api', function(req, res){
+  res.send({ name: 'Yoshi' });
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, function(){
     console.log('now listening for requests');
