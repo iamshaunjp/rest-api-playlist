1e0e3716c2d6758873dce06408adca2662bc469c
Shaun Pelling <shaunjp@shaun-pelling.com>
2017-02-23 13:45:51 +0000

    added lesson 13 end code

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// create geolocation Schema
+const GeoSchema = new Schema({
+    type: {
+        type: String,
+        default: 'Point'
+    },
+    coordinates: {
+        type: [Number],
+        index: '2dsphere'
+    }
+});
+
 // create ninja Schema & model
 const NinjaSchema = new Schema({
     name: {
@@ -13,8 +25,8 @@ const NinjaSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
-    // add in geo location
+    },
+    geometry: GeoSchema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
