6f5d622f6addc51b0afdcfcec1a6d1e84c1d789f
HARSHIT YADAV <56081331+Harshit-1999@users.noreply.github.com>
2020-12-12 01:06:36 +0530

    Fixing the issue number -13 of geoNear Function

--- routes/api.js
+++ routes/api.js
@@ -3,18 +3,21 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // get a list of ninjas from the db
-router.get('/ninjas', function(req, res, next){
-    /* Ninja.find({}).then(function(ninjas){
-        res.send(ninjas);
-    }); */
-    Ninja.geoNear(
-        {type: 'Point', coordinates: [parseFloat(req.query.lng), parseFloat(req.query.lat)]},
-        {maxDistance: 100000, spherical: true}
-    ).then(function(ninjas){
-        res.send(ninjas);
-    }).catch(next);
-});
+router.get("/nin",(req,res,next)=>{
 
+        
+        Ninja.aggregate([{
+            $geoNear:{
+        near:
+        {type:'point',coordinates:[parseFloat(req.query.lng),parseFloat(req.query.lat)]},
+        spherical: true ,maxdistance :100000,distanceField: "dist.calulated"}
+        }]).then(function(ninjas){
+        res.send(ninjas)
+        })
+    
+       
+
+});
 // add a new ninja to the db
 router.post('/ninjas', function(req, res, next){
     Ninja.create(req.body).then(function(ninja){
